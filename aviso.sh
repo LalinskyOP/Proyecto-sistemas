e9cd6486a56bd6fd675da6436539455b15bf0c11
JoseLecha <jose-lecha@hotmail.com>
2016-10-29 17:13:35 -0300

    Add files via upload

--- aviso.sh
+++ aviso.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 deudores=0
-for ((i=1;i<=16;i++)); do  cont=$(grep -w $i historial_pago.txt | grep -o -e "N" | wc -l); if [ $cont -ge 4 ]; then  let deudores=$deudores+1; fi; done
+for ((i=1;i<=16;i++)); do  cont=$(grep -w $i historial_pago.txt | grep -o -e "N" | wc -l); if [ $cont -ge 4 ]; then  let deudores=$deudores+1; mkdir Deudores; nombre=$(grep -wq -e $i propietarios.txt | cut -d ":" -f2); apellido=$(grep -wq -e $i propietarios.txt | cut -d ":" -f3); fecha=$(date +%d/%m/%Y);suman=$(grep -w -e $i historial_pago.txt | grep -o "N" | wc -l); deuda=$(($suman * 2100))echo " $fecha\nApartamento: $i\nNombre: $nombre\nApellido: $apellido\nCantidad de meses a pagar: $suman\nDeuda a pagar: $deuda" > Deudores/"$nombre$apellido%i"; done
 if [ $deudores!=0 ]
 then echo "Hay $deudores inquilinos que deben cuatro meses o m$aacutes"
 sleep 3
--- menuproyecto.sh
+++ menuproyecto.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 clear
-deudores=0
-for ((i=1;i<=16;i++)); do  cont=$(grep -w $i historial_pago.txt | grep -o -e "N" | wc -l); if [ $cont -ge 4 ]; then  let deudores=$deudores+1; fi; done
-if [ $deudores!=0 ]; then echo "Hay $deudores inquilinos que deben cuatro meses o m$aacutes"; sleep 3; clear; fi
-
 while [ "$var" != 3 ]
 do
 echo  " Bienvenidos 
