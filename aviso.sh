8c29b7c55312a133e904f9222e3111d4d804593c
JoseLecha <jose-lecha@hotmail.com>
2016-10-29 18:09:26 -0300

    Add files via upload

--- aviso.sh
+++ aviso.sh
@@ -1,13 +1,47 @@
 #!/bin/bash
 clear
 deudores=0
-for ((i=1;i<=16;i++)); do  cont=$(grep -w $i historial_pago.txt | grep -o -e "N" | wc -l); if [ $cont -ge 4 ]; then  let deudores=$deudores+1; mkdir Deudores; nombre=$(grep -wq -e $i propietarios.txt | cut -d ":" -f2); apellido=$(grep -wq -e $i propietarios.txt | cut -d ":" -f3); fecha=$(date +%d/%m/%Y);suman=$(grep -w -e $i historial_pago.txt | grep -o "N" | wc -l); deuda=$(($suman * 2100))echo " $fecha\nApartamento: $i\nNombre: $nombre\nApellido: $apellido\nCantidad de meses a pagar: $suman\nDeuda a pagar: $deuda" > Deudores/"$nombre$apellido%i"; done
-if [ $deudores!=0 ]
-then echo "Hay $deudores inquilinos que deben cuatro meses o m$aacutes"
-sleep 3
-clear
-'menuproyecto.sh'
+if [ Deudores ]
+then
+	for ((i=1;i<=16;i++))
+	do  
+	cont=$(grep -w $i historial_pago.txt | grep -o -e "N" | wc -l)
+	if [ $cont -ge 4 ]
+	then  
+		let deudores=$deudores+1
+		nombre=$(grep -w -e $i propietarios.txt | cut -d ":" -f2)
+		apellido=$(grep -w -e $i propietarios.txt | cut -d ":" -f3)
+		fecha=$(date +%d/%m/%Y)
+		suman=$(grep -w -e $i historial_pago.txt | grep -o "N" | wc -l)
+		deuda=$(($suman * 2100))
+		echo -e " $fecha\nApartamento: $i\nNombre: $nombre\nApellido: $apellido\nCantidad de meses a pagar: $suman\nDeuda a pagar: $deuda" > Deudores/"$nombre$apellido$i"
+	fi
+	done
 else
-'menuproyecto.sh'
+mkdir Deudores
+for ((i=1;i<=16;i++))
+do  
+cont=$(grep -w $i historial_pago.txt | grep -o -e "N" | wc -l)
+if [ $cont -ge 4 ]
+then  
+	let deudores=$deudores+1
+	nombre=$(grep -w -e $i propietarios.txt | cut -d ":" -f2)
+	apellido=$(grep -w -e $i propietarios.txt | cut -d ":" -f3)
+	fecha=$(date +%d/%m/%Y)
+	suman=$(grep -w -e $i historial_pago.txt | grep -o "N" | wc -l)
+	deuda=$(($suman * 2100))
+	echo -e " $fecha\nApartamento: $i\nNombre: $nombre\nApellido: $apellido\nCantidad de meses a pagar: $suman\nDeuda a pagar: $deuda" > Deudores/"$nombre$apellido$i"
 fi
+done
+fi
+if [ $deudores!=0 ]
+then
+	echo "Hay $deudores inquilinos que deben cuatro meses o mas"
+	sleep 3
+	clear
+	'menuproyecto.sh'
+else
+	'menuproyecto.sh'
+	fi
+
 
